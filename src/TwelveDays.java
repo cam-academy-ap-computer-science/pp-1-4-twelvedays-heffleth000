9976f2bb5b233b32ece542467b9fd913b5a1ef60
heffleth000 <heffleth000@CAM-LAB-17.bgsd.k12.wa.us>
2019-09-17 07:51:15 -0700

    TwelveDays in progress

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -3,7 +3,73 @@ public class TwelveDays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		first();
+		System.out.println("");
+		second();
+		System.out.println("");
+		third();
+		System.out.println("");
+		fourth();
+		System.out.println("");
+		fifth();
+		System.out.println("");
+		sixth();
+		System.out.println("");
+		seventh();
+		System.out.println("");
+		eighth();
+		System.out.println("");
+		ninth();
+		System.out.println("");
+		tenth();
+		System.out.println("");
+		eleventh();
+		System.out.println("");
+		twelfth();
+	}
+	public static void love() {
+		System.out.println("my true love sent to me");
+	}
+	public static void partridge() {
+		System.out.println("a partridge in a pear tree.");
+	}
+	public static void doves() {
+		System.out.println("two turtle doves, and");
+	}
+	public static void hens() {
+		System.out.println("three French hens,");
+	}
+	public static void birds() {
+		System.out.println("four calling birds,");
+	}
+	public static void rings() {
+		System.out.println("five golden rings,");
+	}
+	public static void geese() {
+		System.out.println("six geese a-laying,");
+	}
+	public static void swans() {
+		System.out.println("seven swans a-swimming");
+	}
+	public static void maids() {
+		System.out.println("eight maids a-milking,");
+	}
+	public static void ladies() {
+		System.out.println("nine ladies dancing,");
+	}
+	public static void lords() {
+		System.out.println("ten lords a-leaping,");
+	}
+	public static void pipers() {
+		System.out.println("eveln pipers piping,");
+	}
+	public static void drummers() {
+		System.out.println("Twelve drummers drumming,");
+	}
+	public static void first() {
+		System.out.println("On the first day of Christmas,");
+		love();
+		partridge();
 	}
 
 }
